0d0f8223af1207d5d58cee38af3dcb2189c03095
HashRei <hashrei@protonmail.com>
2023-03-16 14:56:56 +0100

    Remove Sismo redirection

--- src/pages/Account/components/onboarding/onboarding.tsx
+++ src/pages/Account/components/onboarding/onboarding.tsx
@@ -8,16 +8,19 @@ import {
 import { Stack } from '@mui/system';
 import React, { useEffect, useState } from 'react';
 import { OnboardingComponent, OnboardingComponentProps } from '../types';
-import { ZkConnect, ZkConnectClientConfig, ZkConnectResponse } from "@sismo-core/zk-connect-client";
+import {
+  ZkConnect,
+  ZkConnectClientConfig,
+  ZkConnectResponse,
+} from '@sismo-core/zk-connect-client';
 
 const zkConnectConfig: ZkConnectClientConfig = {
   // you will need to register an appId in the Factory
-  appId: "0xc80efc569984bdcf3bb6cbd00c6cae97",
+  appId: '0xc80efc569984bdcf3bb6cbd00c6cae97',
   devMode: {
-    enabled: true
-  }, 
-  vaultAppBaseUrl: "chrome-extension://khdbbdjjghoinfjjiobldgppdemjkomi"
-}
+    enabled: true,
+  },
+};
 
 // create a new ZkConnect instance with the client configuration
 const zkConnect = ZkConnect(zkConnectConfig);
@@ -25,7 +28,6 @@ const zkConnect = ZkConnect(zkConnectConfig);
 const Onboarding: OnboardingComponent = ({
   onOnboardingComplete,
 }: OnboardingComponentProps) => {
-
   const [zkConnectResponse, setZkConnectResponse] =
     useState<ZkConnectResponse | null>(null);
 
@@ -38,10 +40,10 @@ const Onboarding: OnboardingComponent = ({
   }, []);
 
   const requestProof = async () => {
-    // The `request` function sends your user to the Sismo Data Vault App 
+    // The `request` function sends your user to the Sismo Data Vault App
     // to generate the proof of Data Vault ownerhsip.
     zkConnect.request();
-  }
+  };
 
   return (
     <Box sx={{ padding: 2 }}>
@@ -58,7 +60,7 @@ const Onboarding: OnboardingComponent = ({
           <Button
             size="large"
             variant="contained"
-            onClick={() => requestProof()}
+            onClick={() => onOnboardingComplete()}
           >
             Generate Proof
           </Button>
